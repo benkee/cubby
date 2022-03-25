e2f28a23053ac4cba350386081a97b109aafc003
Ben <benkeenan126@gmail.com>
2022-03-25 01:38:30 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Cubby());
+}
+
+// ignore: use_key_in_widget_constructors
+class Cubby extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cubby',
+      theme: ThemeData(
+        primaryColor: Colors.lightGreen,
+        primarySwatch: Colors.lightGreen,
+        canvasColor: Colors.amberAccent,
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(
+            color: Colors.white,
+          )
+        )
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: const [
+              Icon(Icons.restaurant_menu),
+              SizedBox(width: 10),
+              Text('Cubby'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
