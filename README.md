90629e3dc592a684c25ea616a43ec34808856bd2
Ben <benkeenan126@gmail.com>
2022-04-28 02:16:18 +0100

    - Updated README - Added unversioned files

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# cubby
+# Cubby
 
-A new Flutter project.
+A mobile application to help reduce food waste.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Created by Ben Keenan for my dissertation in Computer Science at Newcastle University
---
+++ lib/widgets/food_item_waste_check.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../../models/food_item.dart';
+import '../../services/firebase_crud.dart';
+import '../views/home/home.dart';
+
+// ignore: must_be_immutable
+class FoodItemWasteCheck extends StatefulWidget {
+  late FoodItem foodItem;
+  late String userID;
+  FoodItemWasteCheck({required this.foodItem, required this.userID, Key? key})
+      : super(key: key);
+  @override
+  _FoodItemWasteCheckState createState() => _FoodItemWasteCheckState();
+}
+
+class _FoodItemWasteCheckState extends State<FoodItemWasteCheck> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.amber[300],
+      title: Text(
+        'Are you sure you want\n to waste ${widget.foodItem.name}',
+        textAlign: TextAlign.center,
+        style: const TextStyle(color: Colors.white),
+        overflow: TextOverflow.ellipsis,
+      ),
+      content: const Text(
+        'Note: When you waste the food item, your food waste % will increase.',
+        style: TextStyle(color: Colors.white),
+        textAlign: TextAlign.justify,
+      ),
+      actions: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            FirebaseCRUD.updateUserFoodWasted(widget.userID);
+            FirebaseCRUD.deleteFoodItem(widget.foodItem, widget.userID);
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => HomePage(1)));
+            setState(() {});
+          },
+          child:
+              const Text('Waste Item', style: TextStyle(color: Colors.white)),
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cancel', style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    );
+  }
+}
